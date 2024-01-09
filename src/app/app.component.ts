eb7c5d03bb933804f6768888145e11f13795de6a
Waikikec <martin.d.yankov@gmail.com>
2024-01-09 16:12:24 +0200

    feat: styling components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 import { HeaderComponent } from './layouts/header/header.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    CategoryNavbarComponent,
+    FooterComponent,
+    PostCardComponent,
+    SubscriptionFormComponent,
+  ],
 })
 export class AppComponent {
   title = 'ang-blog-app';
